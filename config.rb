ee3e6095e8ea44cb6c3b11e7dcaf8b5c376064ca
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-06-30 12:18:59 +0100

    Update links

--- config.rb
+++ config.rb
@@ -19,4 +19,4 @@ DESCRIPTION = %q{This Ruby extension provides conversion from Hornetseye::MultiA
 LICENSE = 'GPL-3+'
 AUTHOR = %q{Jan Wedekind}
 EMAIL = %q{jan@wedesoft.de}
-HOMEPAGE = %q{http://wedesoft.github.com/hornetseye-linalg/}
+HOMEPAGE = %q{http://wedesoft.github.io/hornetseye-linalg/}
