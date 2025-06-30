0aa191c3c2688b37bd1bde0fc9678561ca1d5996
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-06-30 12:33:58 +0100

    Update version number

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 require 'rake'
 
 PKG_NAME = 'hornetseye-linalg'
-PKG_VERSION = '1.0.4'
+PKG_VERSION = '1.0.5'
 CFG = RbConfig::CONFIG
 CXX = ENV[ 'CXX' ] || 'g++'
 RB_FILES = ['config.rb'] + FileList[ 'lib/**/*.rb' ]
